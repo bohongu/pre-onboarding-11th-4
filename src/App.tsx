59a1c959eb57a130f1ae0b8b337e71207734a3c1
jaehong <jaehong2782@gmail.com>
2023-07-16 13:04:36 +0900

    Feat: setting router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Search from "./pages/Search";
+import Result from "./pages/Result";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Search />} />
+        <Route path="/:id" element={<Result />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
