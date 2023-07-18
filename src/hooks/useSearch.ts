d4f19cbb60bd842546b84064b9e32f62c16f7b84
jaehong <jaehong2782@gmail.com>
2023-07-18 19:09:38 +0900

    Fix: 무한 렌더링되는 문제로 인한 useFetch 삭제

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,32 +1,65 @@
 import { useCallback, useEffect, useState } from "react";
+import { SickListProps } from "../types/sick";
 import { DEBOUNCE_TIME } from "../constants/debounce";
 import sliceData from "../utils/sliceData";
-import { SickListProps } from "../types/sick";
+import { EXPIRE_SECONDS } from "../constants/cache";
 
 const useSearch = (keyword: string | number) => {
   const [result, setResult] = useState<SickListProps>([]);
-
   const onHandleSearch = useCallback(async () => {
     const URL = `http://localhost:4000/sick?q=${keyword}`;
     const cacheStorage = await caches.open("search");
     const responseCache = await cacheStorage.match(URL);
-    if (keyword !== "") {
+    if (keyword) {
       try {
         if (responseCache) {
           const data = sliceData(await responseCache.json());
           setResult(data);
+
+          const cacheControl = responseCache.headers.get("Cache-Control");
+          const maxAge = cacheControl?.substring(8);
+          const cachedTime = responseCache.headers.get("Cached-Date");
+          const expireTime = Number(cachedTime) + Number(maxAge) * 1000;
+
+          if (expireTime <= Number(Date.now())) {
+            const response = await fetch(URL);
+            const newData = sliceData(await response.clone().json());
+            setResult(newData);
+
+            const headers = new Headers(response.headers);
+            headers.set("Cache-Control", `max-age=${EXPIRE_SECONDS}`);
+            headers.set("Cached-Date", String(Date.now()));
+
+            const modifiedResponse = new Response(response.body, {
+              status: response.status,
+              statusText: response.statusText,
+              headers: headers,
+            });
+            cacheStorage.put(URL, modifiedResponse);
+          }
         } else {
           const response = await fetch(URL);
           const data = sliceData(await response.clone().json());
           setResult(data);
-          cacheStorage.put(URL, response);
-          console.log("calling api");
+
+          const headers = new Headers(response.headers);
+          headers.set("Cache-Control", `max-age=${EXPIRE_SECONDS}`);
+          headers.set("Cached-Date", String(Date.now()));
+
+          const modifiedResponse = new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: headers,
+          });
+
+          cacheStorage.put(URL, modifiedResponse);
+          console.log("calling API");
         }
       } catch (e) {
         console.error(e);
       }
     }
-  }, [keyword, setResult]);
+  }, [keyword]);
 
   useEffect(() => {
     const debounce = setTimeout(() => {
